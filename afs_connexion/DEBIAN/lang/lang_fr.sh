a650c1ec368797d0e8ab89d77aad9694ae27ad5e
PixPix20 <88167062+PixPix20@users.noreply.github.com>
2024-11-23 15:44:33 +0100

    Delete afs_connexion/DEBIAN/lang directory

--- afs_connexion/DEBIAN/lang/lang_fr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-title="Configuration"
-msg_enter_username="Veuillez entrer votre nom d'utilisateur (prenom.nom) de votre compte EPITA :"
-msg_installation_cancelled="Installation annulée."
-msg_installation_complete="Installation terminée."
